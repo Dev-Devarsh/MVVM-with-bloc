27addbcf13cf58553693e6d42db9cc5804c1dc95
dev-devarsh <180130111055@gecg28.ac.in>
2023-01-26 12:36:01 +0530

    .

--- lib/controllers/cubit/user_cubit_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'user_cubit_state.dart';
-
-class UserCubitCubit extends Cubit<UserCubitState> {
-  UserCubitCubit() : super(UserCubitInitial());
-}
--- lib/controllers/cubit/user_cubit_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'user_cubit_cubit.dart';
-
-@immutable
-abstract class UserCubitState {}
-
-class UserCubitInitial extends UserCubitState {}
