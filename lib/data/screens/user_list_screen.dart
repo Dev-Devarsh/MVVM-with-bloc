edee390905b186090f64d0a8b49a606279338cd5
dev-devarsh <180130111055@gecg28.ac.in>
2023-01-26 18:59:55 +0530

    make user list data

--- lib/data/screens/user_list_screen.dart
+++ lib/data/screens/user_list_screen.dart
@@ -1,7 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_task/controllers/cubit/user_cubit.dart';
-import 'package:flutter_task/data/screens/widgets/user_tiles.dart';
+import 'package:flutter_task/data/models/all_user_model.dart';
+import 'package:flutter_task/data/screens/router/router.dart';
+import 'package:flutter_task/data/screens/single_user.dart';
 
 class UserList extends StatefulWidget {
   static const String route = '/userList';
@@ -29,9 +32,59 @@ class _UserListState extends State<UserList> {
       body: BlocBuilder<UserCubit, UserState>(
         builder: (context, state) {
           if (state is GetAllUsersSuccess) {
-            return UserInfo(
-              userData: state.data,
+            List<Datum> data = state.data.data;
+            return ListView.separated(
+              itemCount: data.length,
+              separatorBuilder: (context, index) => const SizedBox(
+                height: 20,
+              ),
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: InkWell(
+                    onTap: () {
+                      showProfile(data[index].avatar);
+                    },
+                    child: CachedNetworkImage(
+                      height: 50,
+                      width: 50,
+                      imageUrl: data[index].avatar,
+                      imageBuilder: ((context, imageProvider) {
+                        return CircleAvatar(
+                          foregroundImage: imageProvider,
+                        );
+                      }),
+                    ),
+                  ),
+                  title: InkWell(
+                    onTap: () {
+                      AppRouter.goToNextPage(context, SingleUser.route,
+                              arguments: {'userId': data[index].id})
+                          .then((value) => _userCubit.getAllUserCubit());
+                    },
+                    child: Row(
+                      children: [
+                        Text(
+                          data[index].firstName,
+                          style: TextStyle(fontSize: 23),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          data[index].lastName,
+                          style: TextStyle(fontSize: 23),
+                        ),
+                      ],
+                    ),
+                  ),
+                  subtitle: Text(
+                    data[index].email,
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w700),
+                  ),
+                );
+              },
             );
+            ;
           }
           if (state is GetAllUsersFailed) {
             return const Center(
@@ -43,6 +96,24 @@ class _UserListState extends State<UserList> {
           return const Center(child: CircularProgressIndicator());
         },
       ),
+    );
+  }
+
+  showProfile(String url) {
+    return showDialog(
+      barrierDismissible: true,
+      context: context,
+      builder: (context) {
+        return Container(
+          height: 60,
+          width: 60,
+          child: CachedNetworkImage(
+            height: 60,
+            width: 60,
+            imageUrl: url,
+          ),
+        );
+      },
     );
   }
 }
