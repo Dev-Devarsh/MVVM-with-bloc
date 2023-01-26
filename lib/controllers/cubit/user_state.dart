5fe0bb6260b319c9592189f3bf1b3c72b0e0e2e1
dev-devarsh <180130111055@gecg28.ac.in>
2023-01-26 15:15:52 +0530

    added states in cubit

--- lib/controllers/cubit/user_state.dart
+++ lib/controllers/cubit/user_state.dart
@@ -4,3 +4,19 @@ part of 'user_cubit.dart';
 abstract class UserState {}
 
 class UserInitial extends UserState {}
+
+class Loading extends UserState {}
+
+class GetAllUsersSuccess extends UserState {
+  final AllUserData data;
+  GetAllUsersSuccess({required this.data});
+}
+
+class GetSingleUsersSuccess extends UserState {
+  final SingleUserData data;
+  GetSingleUsersSuccess({required this.data});
+}
+
+class GetAllUsersFailed extends UserState {}
+
+class GetSingleUserFailed extends UserState {}
